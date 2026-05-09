550df1567189a857ae7b3160ee3c306fcd9119d6
SHINYOUNGHO94 <shinyoungho94@gmail.com>
2026-05-09 22:07:14 +0900

    yarn lint エラー修正

--- packages/task-app/src/components/dashboard/Sidebar.tsx
+++ packages/task-app/src/components/dashboard/Sidebar.tsx
@@ -3,8 +3,6 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { 
   LayoutDashboard, 
-  CheckSquare,
-  Settings, 
   ChevronRight,
   TreeDeciduous,
   Building2,
